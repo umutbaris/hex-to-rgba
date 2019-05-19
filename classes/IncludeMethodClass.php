f9152acdeab65742b7a0732f5e7b9c3326be406e
Karasar, Umut <umut.karasar@sony.com>
2019-05-19 21:24:27 +0300

    master Indentation fix

--- classes/IncludeMethodClass.php
+++ classes/IncludeMethodClass.php
@@ -1,41 +1,41 @@
 <?php
 
 class IncludeMehodClass{
-/**
- * Hex values converting to rgba
- *
- * @param string $color
- * @param float $alpha
- * @return string
- */
-public function convertHexToRgba( string $color, float $alpha ):string {
-	if(empty($color) || empty($alpha))
-	throw new Exception('Please enter a color and alpha vlaue');
+	/**
+	 * Hex values converting to rgba
+	 *
+	 * @param string $color
+	 * @param float $alpha
+	 * @return string
+	 */
+	public function convertHexToRgba( string $color, float $alpha ):string {
+		if(empty($color) || empty($alpha))
+		throw new Exception('Please enter a color and alpha vlaue');
 
-	if ($color[0] == '#' ) {
-		$color = substr( $color, 1 );
-	}
+		if ($color[0] == '#' ) {
+			$color = substr( $color, 1 );
+		}
 
-	if (strlen($color) == 6) {
-		$hex = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
-	} elseif ( strlen( $color ) == 3 ) {
-		$hex = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
-	} else {
-		throw new Exception('value is not valid');
-	}
+		if (strlen($color) == 6) {
+			$hex = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
+		} elseif ( strlen( $color ) == 3 ) {
+			$hex = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
+		} else {
+			throw new Exception('value is not valid');
+		}
 
-	$rgb =  array_map('hexdec', $hex);
+		$rgb =  array_map('hexdec', $hex);
 
-	if($alpha >= 1){
-		$alpha = 1.0;
-	} else {
-		$alpha = substr( $alpha, 1 );
-	}
+		if($alpha >= 1){
+			$alpha = 1.0;
+		} else {
+			$alpha = substr( $alpha, 1 );
+		}
 
-	$rgba = 'rgba('.implode(",",$rgb).','.$alpha.')';
+		$rgba = 'rgba('.implode(",",$rgb).','.$alpha.')';
 
-	return $rgba;
+		return $rgba;
 	}
 }
-$class = new IncludeMehodClass();
-echo $class->convertHexToRgba('FFFFFF', 1 );
+	$class = new IncludeMehodClass();
+	echo $class->convertHexToRgba('FFFFFF', 1 );
